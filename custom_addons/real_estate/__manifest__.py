9b56e0c1c3dee53bcb0e3bc5b1620fac67de4444
Milos Pejovic <milos1pejovic@gmail.com>
2025-09-12 15:03:29 +0200

    Initial commit

---
+++ custom_addons/real_estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Milos Pejovic",
+    'category': 'Category',
+    'description': """
+    Practice module made by following the documentation on Odoo website https://www.odoo.com/documentation/18.0/developer/tutorials/server_framework_101/02_newapp.html
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+    'application': True,
+}
