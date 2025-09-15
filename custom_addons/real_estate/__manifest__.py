98b269108a926ca64f851f618abced04799c8a22
Milos Pejovic <milos1pejovic@gmail.com>
2025-09-15 10:20:50 +0200

    Model updates
    - Add new property model fields
    - Add property model access rights

--- custom_addons/real_estate/__manifest__.py
+++ custom_addons/real_estate/__manifest__.py
@@ -1,19 +1,20 @@
 {
-    'name': "Real Estate",
-    'version': '1.0',
-    'depends': ['base'],
-    'author': "Milos Pejovic",
-    'category': 'Category',
-    'description': """
+    "name": "Real Estate",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Milos Pejovic",
+    "category": "Category",
+    "description": """
     Practice module made by following the documentation on Odoo website https://www.odoo.com/documentation/18.0/developer/tutorials/server_framework_101/02_newapp.html
     """,
     # data files always loaded at installation
-    'data': [
-        'views/mymodule_view.xml',
+    "data": [
+        "views/mymodule_view.xml",
+        "security/ir.model.aacess.csv"
     ],
     # data files containing optionally loaded demonstration data
-    'demo': [
-        'demo/demo_data.xml',
+    "demo": [
+        "demo/demo_data.xml",
     ],
-    'application': True,
+    "application": True,
 }
--- custom_addons/real_estate/models/property.py
+++ custom_addons/real_estate/models/property.py
@@ -1,4 +1,32 @@
-from odoo import models
+from odoo import models, fields
+
 
 class Property(models.Model):
     _name = "estate.property"
+    _description = "Real estate property model"
+    _order = "sequence"
+
+    # These attributes are availiable to all field types:
+    # string (str, default: field’s name)
+    # The label of the field in UI (visible by users).
+
+    # required (bool, default: False)
+    # If True, the field can not be empty. It must either have a default value or always be given a value when creating a record.
+
+    # help (str, default: '')
+    # Provides long-form help tooltip for users in the UI.
+
+    # index (bool, default: False)
+
+    name = fields.Char("Property name", required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Post code")
+    date_availability = fields.Date("Date availability")
+    expected_price = fields.Float("Expected price", required=True)
+    selling_price = fields.Float("Selling price")
+    bedrooms = fields.Integer("Bedrooms")
+    living_area = fields.Integer("LIving area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage", default=False)
+    garden_area = fields.Integer("Garden area")
+    garden_orientation = fields.Selection(string="Garder orientation", selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")])
