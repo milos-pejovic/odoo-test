85010785ed5b0bc53c6b9f7b781045ef485ee434
Milos Pejovic <milos1pejovic@gmail.com>
2025-10-26 15:13:46 +0100

    Add Tasks module

---
+++ custom_addons/tasks/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Tasks",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Milos Pejovic",
+    "category": "Category",
+    "description": """
+    """,
+    # data files always loaded at installation
+    "data": [
+        "security/ir.model.access.csv",
+        
+        # Actions first
+        "views/tasks_views.xml",
+
+        # Then the menu that references it
+        "views/tasks_menus.xml"
+    ],
+    # data files containing optionally loaded demonstration data
+    "demo": [
+        "demo/demo_data.xml",
+    ],
+    "application": True,
+    "installable": True
+}
---
+++ custom_addons/tasks/models/tasks_task.py
@@ -0,0 +1,45 @@
+from odoo import models, fields, api
+
+class TasksTask(models.Model):
+    _name = "tasks.task"
+    _description = "Task"
+
+    title = fields.Char(string="Title", required=True)
+    description = fields.Char(string="Description")
+
+    status = fields.Selection(string="Status", selection=[
+        ("1_groomed", "Groomed"),
+        ("2_in_progress", "In progress"),
+        ("3_testing", "Testing"),
+        ("4_on_hold", "On hold"),
+        ("5_done", "Done")
+    ], default="groomed")
+
+    type = fields.Selection(string="Type", selection=[
+        ("backend", "Backend"),
+        ("frontend", "Frontend/template"),
+        ("website", "Website"),
+        ("usage", "Usage"),
+        ("other", "Other"),
+        ("research", "Research")
+    ])
+
+    priority = fields.Selection(string="Priority", selection=[
+        ("low", "Low"),
+        ("medium", "Medium"),
+        ("high", "High")
+    ])
+
+    status_sequence = fields.Integer("Status Sequence", compute="_compute_status_sequence", store=True)
+
+    @api.depends("status")
+    def _compute_status_sequence(self):
+        sequence_map = {
+            "groomed": 1,
+            "in_progress": 2,
+            "testing": 3,
+            "on_hold": 4,
+            "done": 5,
+        }
+        for rec in self:
+            rec.status_sequence = sequence_map.get(rec.status, 99)
